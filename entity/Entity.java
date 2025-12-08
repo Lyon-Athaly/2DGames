552fe7177a363629e0e7d3ec3793915773be0262
Lyon-Athaly <jonathanerhandoyo18@.com>
2025-12-08 19:46:04 +0700

    add Collision

--- entity/Entity.java
+++ entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
@@ -12,4 +13,6 @@ public class Entity {
 
     public int spriteCounter = 0;
     public int spriteNum = 1;
+    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+    public boolean collisionOn = false;
 }
